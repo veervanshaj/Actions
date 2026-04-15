3e7da26617e1e994472edac536766f8928412f69
Veer <veervanshajwadehra@gmail.com>
2026-04-15 11:58:46 +0530

    added a one more step

--- notes.js
+++ notes.js
@@ -1,4 +1,13 @@
 // workflow-> file whcih store Instruction/steps for Ci/cd
 /**
- * 
+ * event -- when to run this workflow like when push ,pr .main(specific branch) , ona perticular schdule or any specific condition
+        using on keyword
+ * Job -- what to do 
+ * Steps -- it is a list          //cooking instrauctions  
+ *
+ */
+
+/** GitHub actins
+ * push->workflow->start job(VM create can be ubantu windos mac etc)->job Completed -> steps
+ * Each job have independent VM
  */
